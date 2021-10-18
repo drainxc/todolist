6e6c7752335361055eb2f9f3fe5c357bbc2478d3
ldh7228 <ldh7228@gmail.com>
2021-10-18 20:47:27 +0900

    [DESIGN] box-shadow
    box-shadow 추가

--- src/components/todoListTemplate/styles.js
+++ src/components/todoListTemplate/styles.js
@@ -1,9 +1,12 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const MainDiv = styled.div`
-    background-color: white;
-    width: 400px;
-    height:550px;
-    display: flex;
-    border-radius: 6px;
-`
+  background-color: white;
+  width: 400px;
+  height: 550px;
+  display: flex;
+  border-radius: 6px;
+  box-shadow: 0 0 4px #FFFFFF,
+    0 0 7px #FFFFFF,
+    0 0 70px #E8B4FF;
+`;
