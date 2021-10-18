a9edb254a10ca397c40a88d465865aa5e906f3b0
ldh7228 <ldh7228@gmail.com>
2021-10-18 19:13:48 +0900

    [ADD] TodoListItem
    TodoListItem 추가

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import TodoListItem from "../todoListItem";
 import TodoListTemplate from "../todoListTemplate";
 
 export default function Container() {
   return (
     <>
-      <TodoListTemplate/>
+      <TodoListTemplate />
+      <TodoListItem />
     </>
   );
 }
