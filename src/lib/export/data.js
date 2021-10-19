3fab60d2c4c1483c159b8b33578afb1fa786e663
ldh7228 <ldh7228@gmail.com>
2021-10-19 20:09:42 +0900

    [ADD] data
    data 추가

---
+++ src/lib/export/data.js
@@ -0,0 +1,28 @@
+export const data = [
+    {
+        contents: "밥 먹기",
+        checked: true,
+        id: 1,
+    },
+    {
+        contents: "공부하기",
+        checked: false,
+        id: 2,
+    },
+    {
+        contents: "게임하기",
+        checked: true,
+        id: 3,
+    },
+    {
+        contents: "쓸 거 없어서 쓰는 란",
+        checked: true,
+        id: 4,
+    },
+    {
+        contents: "아ㅏㅏ아아ㅏ아아아ㅏ아아아ㅏ아ㅏ아아ㅏ아",
+        checked: false,
+        id: 5,
+    },
+    
+]
