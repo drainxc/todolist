47ac3573b053dae0bb352a05e9a1772572e370c7
ldh7228 <ldh7228@gmail.com>
2021-10-19 15:39:15 +0900

    [ADD] header

--- src/components/todoListTemplate/index.js
+++ src/components/todoListTemplate/index.js
@@ -1,11 +1,16 @@
 import React from "react";
-import * as S from "./styles"
+import * as S from "./styles";
+import search from "../../asset/search.png";
 
 export default function TodoListTemplate() {
   return (
     <>
       <S.MainDiv>
-        
+        <header>
+          <div className="day">Tuesday.oct 19</div>
+          <span className="title">To-Do list</span>
+        </header>
+        <hr />
       </S.MainDiv>
     </>
   );
--- src/components/todoListTemplate/styles.js
+++ src/components/todoListTemplate/styles.js
@@ -6,7 +6,32 @@ export const MainDiv = styled.div`
   height: 550px;
   display: flex;
   border-radius: 6px;
-  box-shadow: 0 0 4px #FFFFFF,
-    0 0 7px #FFFFFF,
-    0 0 70px #E8B4FF;
+  box-shadow: 0 0 4px #ffffff, 0 0 7px #ffffff, 0 0 20px #e8b4ff;
+
+  header {
+    width: 100%;
+    padding: 20px;
+
+    .day {
+      color: gray;
+    }
+
+    .title {
+      margin-top: 5px;
+      display: flex;
+      justify-content: flex-start;
+      width: 100%;
+      font-size: 30px;
+      font-weight: 1000;
+    }
+  }
+
+  hr {
+    position: absolute;
+    margin-top: 90px;
+    width: 400px;
+    height: 1px;
+    border: none;
+    background-color: whitesmoke;
+  }
 `;
