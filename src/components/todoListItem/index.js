9864d37d2f6fc0ac4e4ef0112e1bf77c177c5ed0
ldh7228 <ldh7228@gmail.com>
2021-10-19 20:10:49 +0900

    [ADD] data
    TodoListItem에 data 적용

--- src/components/todoListItem/index.js
+++ src/components/todoListItem/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import * as S from "./styles";
 
-export default function TodoListItem() {
+export default function TodoListItem({ contents, checked, key }) {
   return (
     <S.List>
       <span>
         <span className="bullet">•</span>
-        <span className="contents">ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴ</span>
+        <span className="contents">{contents}</span>
       </span>
-      <input type="checkbox" />
+      <input type="checkbox" checked={checked} />
     </S.List>
   );
 }
--- src/components/todoListTemplate/index.js
+++ src/components/todoListTemplate/index.js
@@ -2,13 +2,21 @@ import React from "react";
 import Header from "../todoListHeader";
 import TodoListItem from "../todoListItem";
 import * as S from "./styles";
+import { data } from "../../lib/export/data"
 
 export default function TodoListTemplate() {
+  console.log(data)
   return (
     <>
       <S.MainDiv>
         <Header/>
-        <TodoListItem/>
+        {data.map((item) => (
+          <TodoListItem 
+            contents={item.contents}
+            checked={item.checked}
+            key={item.id}
+          />
+        ))}
       </S.MainDiv>
     </>
   );
