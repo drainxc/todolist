e497d05b579d6f21e5ea025a121f6782c465ab69
ldh7228 <ldh7228@gmail.com>
2021-11-01 20:24:39 +0900

    [ADD] fix event

--- src/components/todoListItem/index.js
+++ src/components/todoListItem/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import * as S from "./styles";
 
-export default function TodoListItem({ contents, checked, key }) {
+export default function TodoListItem({ contents, checked, fix }) {
   return (
     <S.List>
         <span className="bullet">•</span>
+        {fix ? <input value={contents} /> : 
         <span className="contents">{contents}
         <div className="tooltip">{contents}</div>
-        </span>
+        </span>}
       <input type="checkbox" defaultChecked={checked}/>
     </S.List>
   );
--- src/components/todoListTemplate/List/index.js
+++ src/components/todoListTemplate/List/index.js
@@ -3,17 +3,23 @@ import binImg from "../../../asset/bin.png";
 import fixImg from "../../../asset/pen.png";
 import * as S from "./style";
 import TodoListItem from "../../todoListItem";
+import { useState } from "react/cjs/react.development";
 
 function List({ data, onRemove }) {
+  const [fix, setFix] = useState(false);
+  function onFix() {
+    setFix(true);
+  }
+
   return (
     <>
       <span className="item">
         <TodoListItem
           contents={data.contents}
           checked={data.checked}
-          key={data.id}
+          fix={fix}
         />
-        <img className="fix" src={fixImg} alt="" />
+        <img className="fix" src={fixImg} alt="" onClick={onFix}/>
         <S.Button>
           <img onClick={() => onRemove(data.id)} src={binImg} alt="" />
         </S.Button>
